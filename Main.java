8d65c82ba6dced22cb54064b1c0e4ddeeead2e51
admin <has.mehran@yahoo.com>
2018-11-14 16:38:09 +0330

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,17 +14,20 @@ public class Main {
 
         Drug drug1 = new Drug("antihistamine" , 1000);
         Drug drug2 = new Drug("acetaminophen" , 2000);
+        Drug drug3 = new Drug("Lorazepam",1500);
 
         store.setDrugs(drug1);
         store.setDrugs(drug2);
+        store.setDrugs(drug3);
 
         System.out.println(store.getDrugs()[0].getName() + " price is : "
                             + store.getDrugs()[0].getPrice());
         System.out.println(store.getDrugs()[1].getName() + " price is : "
                 + store.getDrugs()[1].getPrice());
+        System.out.println(store.getDrugs()[2].getName() + " price is : "
+                + store.getDrugs()[2].getPrice());
 
-
-        Prescription prescription = doctor.writePrescriptions(drug1,drug2);
+        Prescription prescription = doctor.writePrescriptions(drug1,drug3);
 
         drugStore.employee1.calculatePrescriptionPrice(prescription);
 
